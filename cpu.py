7e29af6db1089e86576c68fdd819b2863eff223e
Julie Jonak <juliejonak10@gmail.com>
2019-08-23 08:55:18 -0700

    Adds ALU operations

--- cpu.py
+++ cpu.py
@@ -12,6 +12,13 @@ CMP = 0b10100111 # ALU command
 JMP = 0b01010100
 JEQ = 0b01010101
 JNE = 0b01010110
+XOR = 0b10101011 # ALU command
+AND = 0b10101000 # ALU command
+OR  = 0b10101010 # ALU command
+NOT = 0b01101001 # ALU command
+SHL = 0b10101100 # ALU command
+SHR = 0b10101101 # ALU command
+
 
 class CPU:
     """Main CPU class."""
@@ -47,7 +54,13 @@ class CPU:
             0b10100111: self.cmp_func,
             0b01010100: self.jmp,
             0b01010101: self.jeq,
-            0b01010110: self.jne
+            0b01010110: self.jne,
+            0b10101011: self.xor,
+            0b10101000: self.and_func,
+            0b10101010: self.or_func,
+            0b01101001: self.not_func,
+            0b10101100: self.shl,
+            0b10101101: self.shr
         }
 
     def __repr__(self):
@@ -117,7 +130,7 @@ class CPU:
             self.PC = self.reg[operand_a]
             # If PC jumps, do not increment
             return(0, True)
-        return(2, True)
+        return (2, True)
 
     def jne(self, operand_a, operand_b):
         # Sets PC to address stored in given register IF equal flag is False
@@ -125,7 +138,37 @@ class CPU:
             self.PC = self.reg[operand_a]
             # If PC jumps, do not increment
             return(0, True)
-        return(2, True)
+        return (2, True)
+
+    def xor(self, operand_a, operand_b):
+        # Performs bitwise XOR
+        self.alu("XOR", operand_a, operand_b)
+        return (3, True)
+
+    def and_func(self, operand_a, operand_b):
+        # Performs bitwise AND
+        self.alu("AND", operand_a, operand_b)
+        return (3, True)
+
+    def or_func(self, operand_a, operand_b):
+        # Performs bitwise OR
+        self.alu("OR", operand_a, operand_b)
+        return (3, True)
+
+    def not_func(self, operand_a, operand_b):
+        # Performs bitwise NOT
+        self.alu("NOT", operand_a, operand_b)
+        return (2, True)
+
+    def shl(self, operand_a, operand_b):
+        # Shifts A to the left by number of bits B
+        self.alu("SHL", operand_a, operand_b)
+        return (3, True)
+
+    def shr(self, operand_a, operand_b):
+        # Shifts A to the left by number of bits B
+        self.alu("SHR", operand_a, operand_b)
+        return (3, True)
 
     def load(self, program):
         """Load a program into memory."""
@@ -163,7 +206,6 @@ class CPU:
 
         elif op == "MUL":
             self.reg[reg_a] = (self.reg[reg_a]) * (self.reg[reg_b])
-            return 2
         
         elif op == "CMP":
             if self.reg[reg_a] > self.reg[reg_b]:
@@ -172,6 +214,31 @@ class CPU:
                 self.FL = 0b00000100
             elif self.reg[reg_a] == self.reg[reg_b]:
                 self.FL = 0b00000001
+        
+        elif op == "XOR":
+            self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
+            
+        elif op == "AND":
+            self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+
+        elif op == "OR":
+            self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
+        
+        elif op == "NOT":
+            # Because Python uses signed numbers, ~ (toggling bits) doesn't work every time.
+            # Python simulates an infinite bit representation (not 8, 32, etc) so toggling also affects the unseen infinite leading 0's (which becomes 1's). When cut down to an absolute (abs()), it can become a number different than the expected NOT outcome
+            # Workaround is to subtract from 0b11111111
+            # https://stackoverflow.com/questions/31151107/how-do-i-do-a-bitwise-not-operation-in-python
+            # https://stackoverflow.com/questions/46573219/the-meaning-of-bit-wise-not-in-python
+            self.reg[reg_a] = 0b11111111 - self.reg[reg_a]
+
+        elif op == "SHL":
+            # Does this auto-fill low bits with 0s?
+            self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
+
+        elif op == "SHR":
+            # Does this auto-fill high bits with 0s?
+            self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
 
         else:
             raise Exception("Unsupported ALU operation")
